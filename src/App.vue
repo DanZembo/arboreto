198b70b300bb09dde2e0c1befb67922bbd0a9d00
DanZembo <obchod@zidle-servis.cz>
2024-09-16 16:27:38 +0200

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app" class="p-6">
+    <div class="container mx-auto">
+      <ProjectInfoComponent />
+      <PropertySelectionComponent />
+      <RecordsTableComponent />
+      <MobileWorkComponent />
+    </div>
+  </div>
+</template>
+
+<script>
+import ProjectInfoComponent from "./components/ProjectInfoComponent.vue";
+import PropertySelectionComponent from "./components/PropertySelectionComponent.vue";
+import RecordsTableComponent from "./components/RecordsTableComponent.vue";
+import MobileWorkComponent from "./components/MobileWorkComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    ProjectInfoComponent,
+    PropertySelectionComponent,
+    RecordsTableComponent,
+    MobileWorkComponent,
+  },
+};
+</script>
+
+<style>
+@import "https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css";
+</style>
