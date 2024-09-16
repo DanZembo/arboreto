c187651260080316a9578226494fe46010d98bfc
DanZembo <obchod@zidle-servis.cz>
2024-09-16 16:33:51 +0200

    Create RecordsTableComponent.vue

---
+++ src/components/RecordsTableComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h2>Editace projektu</h2>
+    <table>
+      <thead>
+        <tr>
+          <th v-for="property in properties" :key="property">{{ property }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td v-for="property in properties" :key="property">
+            <input v-model="newRow[property]" type="text">
+          </td>
+        </tr>
+        <tr v-for="(row, rowIndex) in rows" :key="rowIndex">
+          <td v-for="property in properties" :key="property">{{ row[property] }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      properties: ["Název taxonu česky", "Výška", "Průměr kmene"],
+      rows: [],
+      newRow: {}
+    };
+  },
+  methods: {
+    addRow() {
+      this.rows.push({ ...this.newRow });
+      this.newRow = {};
+    }
+  }
+};
+</script>
